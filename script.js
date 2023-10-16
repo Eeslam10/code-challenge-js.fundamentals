99f408f2e6086ecb857ae5cdf2e97628741f7258
islam sani <islammuhd8@gmail.com>
2023-10-16 10:39:37 +0100

    added a canIVote function

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ const greetWorld = () => {
   };
 
 // 2.
+// The most common minimum age to vote is 18. Write a function canIVote() that takes in a number, representing the person’s age, and returns the boolean true if they are 18 years old or older, and the boolean false if they are not.
+
+// Write your function here:
+const canIVote = age => {
+    if (age >= 18) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
