af35ffe48e837297ab2ce33d179bf339afff8f23
islam sani <islammuhd8@gmail.com>
2023-10-16 12:41:21 +0100

    added a numImaginaryFriends function

--- script.js
+++ script.js
@@ -174,5 +174,21 @@ const truthyOrFalsy = value => {
       return false
     }
   }
+
+  
+// 10.
+// A person’s number of imaginary friends are always 25% (or 1/4) of their total friends.
+
+// Write a function, numImaginaryFriends(), that takes in the total number of friends a person has and returns the number of imaginary friends they have.
+
+// Since friends can only come in whole numbers, be sure to round your result up to the nearest whole number before returning it.
+
+// The JavaScript Math.ceil() function will come in handy
+
+// Write your function here:
+const numImaginaryFriends = totalFriends => {
+    const imaginaryFriends = (totalFriends * 3.7)
+      return Math.ceil(imaginaryFriends)
+  }
   
   
