16dc16092c65ffdbc7f50591d7fef8bc861b6406
islam sani <islammuhd8@gmail.com>
2023-10-16 19:33:09 +0100

    added tipCalculator function

--- script.js
+++ script.js
@@ -274,3 +274,29 @@ console.log(whatRelation(34))
 
 console.log(whatRelation(3))
 // Should print 'You are likely 2nd cousins.'
+
+// 14.
+// Create a function, tipCalculator(), that has two parameters, a string representing the quality of the service received and a number representing the total cost.
+
+// Return the tip, as a number, based on the following:
+// ‘bad’ should return a 5% tip
+// ‘ok’ should return a 15% tip
+// ‘good’ should return a 20% tip
+// ‘excellent’ should return a 30% tip
+// all other inputs should default to 18%
+
+const tipCalculator = (quality, total) => {
+    switch (quality) {
+        case 'bad':
+            return total * .05;
+        case 'ok':
+            return total * .15;
+        case 'good':
+            return total * .20;
+        case 'excellent':
+            return total * .30;
+        default:
+            return total * .18;
+    }
+
+console.log(tipCalculator('good', 100))
